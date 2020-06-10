850d18d51b5d2923f6d57f2a068028b051f2f546
Julie <julienne.bay@gmail.com>
2020-06-10 12:12:48 -0400

    rendered the students list (which can be searched by their names)  &  added basic styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import students from "./students";
+
+function Student({ student }) {
+  const [showGrades, setShowGrades] = useState(false);
+
+  function toggleGrade() {
+    setShowGrades(!showGrades);
+  }
+
+  return (
+    <>
+      <div className="icon-container">
+        <img src={student.pic} alt={student.firstName} />
+      </div>
+      <div className="student-profile">
+        <h3>
+          name: {student.firstName} {student.lastName}
+        </h3>
+        <h4>email: {student.email}</h4>
+        <h4>company: {student.company}</h4>
+        <h4>
+          average grade:
+          {Math.round(
+            student.grades.map(str => Number(str)).reduce((a, b) => a + b, 0) /
+              student.grades.length
+          )}
+          %
+        </h4>
+        {showGrades ? (
+          <div className="complete-grades">
+            <ul>
+              {student.grades.map((grade, i) => (
+                <li>
+                  Test {i + 1}: {grade} %
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+      <div className="plus-minus">
+        <button onClick={toggleGrade}>Toggle grades</button>
+      </div>
+    </>
+  );
+}
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const updateSearchTerm = event => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={updateSearchTerm}
+          placeholder="search student by name"
+        />
+      </div>
+      {students
+        .filter(student =>
+          `${student.firstName} ${student.lastName}`
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+        )
+        .map(student => (
+          <div className="student-container">
+            <Student student={student} />
+          </div>
+        ))}
     </div>
   );
 }
