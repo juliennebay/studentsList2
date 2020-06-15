9b8a7028eb0b3282119840419d8765cf8ae4b51c
Julie <julienne.bay@gmail.com>
2020-06-15 14:29:05 -0400

    Updates

---
+++ precache-manifest.2a7615b50fc4bf4d1efa7c9cca7931fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c23b954c012135a639e4756dad2dc8",
+    "url": "/studentsList2/index.html"
+  },
+  {
+    "revision": "7d0cff704b89d1f9be39",
+    "url": "/studentsList2/static/css/main.3e7ddcb6.chunk.css"
+  },
+  {
+    "revision": "36a33bdeeeb2f441510b",
+    "url": "/studentsList2/static/js/2.311169aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/studentsList2/static/js/2.311169aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0cff704b89d1f9be39",
+    "url": "/studentsList2/static/js/main.e43b2ab5.chunk.js"
+  },
+  {
+    "revision": "49c52a7a80eb9bdd4a9a",
+    "url": "/studentsList2/static/js/runtime-main.df38df83.js"
+  }
+]);
